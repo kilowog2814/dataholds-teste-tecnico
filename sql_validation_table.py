c8c61cc28093755efba8cf3f503d180f7c42190b
Cleiton Aleixo <cleitn1@gmail.com>
2024-08-28 10:34:45 -0300

    ajuste na validação incluindo as novas tabelas

--- sql_validation_table.py
+++ sql_validation_table.py
@@ -14,28 +14,34 @@ POSTGRE_PWD = os.getenv("PASSWORD")
 conn = None
 cur = None
 
+tables_validation_list = [
+    {"table": "dim_produto", "sql_file": "create_dim_produtos.sql"},
+    {"table": "dim_cliente", "sql_file": "create_dim_clientes.sql"},
+    {"table": "fact_sales_with_dates", "sql_file": "create_sales_data_with_dates.sql"},
+]
 
-def truncate_table(cur, logger):
+
+def truncate_table(cur, logger, table_name):
     try:
-        logger.info("apagando dados da tabela...")
-        cur.execute("TRUNCATE TABLE public.sales_data_with_dates")
-        logger.success("dados apagados com sucesso.")
+        cur.execute(f"TRUNCATE TABLE public.{table_name} RESTART IDENTITY CASCADE")
+
+        logger.success(f"dados da tabela {table_name} apagados com sucesso.")
     except Exception as e:
         logger.exception(e)
         raise
 
 
-def create_table(cur, logger):
+def create_table(cur, logger, file_name_sql):
     try:
-        logger.info("tabela não existe no banco, criando a tabela...")
-        cur.execute(open("create_sales_data_with_dates.sql", "r").read())
+
+        cur.execute(open(file_name_sql, "r").read())
         logger.info("tabela criada com sucesso.")
     except Exception as e:
         logger.exception(e)
         raise
 
 
-def validarTabela(logger):
+def validation_tables(logger):
 
     try:
         conn = psycopg2.connect(
@@ -48,23 +54,29 @@ def validarTabela(logger):
         conn.autocommit = True
         cur = conn.cursor()
 
-        cur.execute(
-            """
-            SELECT EXISTS (
-            SELECT FROM information_schema.tables 
-            WHERE table_schema = 'public' 
-            AND table_name = %s
-            );
-        """,
-            ("sales_data_with_dates",),
-        )
+        for table in tables_validation_list:
 
-        table_exist = cur.fetchone()[0]
+            cur.execute(
+                """
+                SELECT EXISTS (
+                SELECT FROM information_schema.tables 
+                WHERE table_schema = 'public' 
+                AND table_name = %s
+                );
+            """,
+                (table["table"],),
+            )
 
-        if not table_exist:
-            create_table(cur, logger)
-        else:
-            truncate_table(cur, logger)
+            table_exist = cur.fetchone()[0]
+
+            if not table_exist:
+                logger.info(
+                    f"tabela {table['table']} não existe no banco, criando a tabela..."
+                )
+                create_table(cur, logger, table["sql_file"])
+            else:
+                logger.info(f"apagando dados da tabela {table['table']}...")
+                truncate_table(cur, logger, table["table"])
 
     except Exception as e:
         logger.exception(e)
